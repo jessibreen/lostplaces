da6295d2a07dcd99ff474d263d247b025b7a296b
jessibreen <jessibreen@gmail.com>
2017-03-25 14:57:17 -0400

    current state of affairs

--- js/app.js
+++ js/app.js
@@ -11,26 +11,18 @@ var map = new mapboxgl.Map({
     bearing: 48.05
 });
 
-var geoJsonFeatures;
-var Draw = mapboxgl.Draw(
-    {
-        displayControlsDefault: false,
-        controls: {
-            'point': true
-            'trash': true
-        },
 
-    }
-);
+var geoJsonFeatures;
+var Draw = new MapboxDraw({
+    displayControlsDefault: false,
+    controls: {
+        'point': true,
+        'trash': true
+    },
+
+});
 
 var datasetId = "cj0ldpqfa00022xphwg2k355z";
-
-var source = new mapboxgl.GeoJSONSource({
-    data: {
-        "type": "FeatureCollection",
-        "features": []
-    }
-});
 
 document.getElementById('updateDataset').onclick = function(e) {
     e.preventDefault();
@@ -41,21 +33,70 @@ document.getElementById('updateDataset').onclick = function(e) {
 map.on('load', function(){
     map.addControl(Draw);
 
-    map.addSource('dataset', source);
+
+    map.addSource('dataset', {
+        "type": "geojson",
+        "data":  {
+            type: 'FeatureCollection',
+            features: []
+        }
+        }
+    );
+
     map.addLayer({
         "id": "dataset-point",
-        "type": "circle",
+        "type": "symbol",
         "source": "dataset",
-        "layout": {},
-        "paint": {
-            // grab the route's color value
-            "circle-color": "#73b6e6",
+        "layout": {
+            "icon-image": "triangle-15"
         },
+        //"paint": {
+            // grab the route's color value
+            //"circle-color": "#73b6e6",
+        //},
         "filter": [
             "all",
             ["==", "$type", "Point"]
         ]
+});
+
+    map.on('click', function (e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['dataset-point'] });
+
+        if (!features.length) {
+            return;
+        }
+
+        var feature = features[0];
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        var popup = new mapboxgl.Popup()
+            .setLngLat(feature.geometry.coordinates)
+            .setHTML("<h1>Hello World!</h1>")
+            .addTo(map);
     });
+
+    // map.on('click', function (e) {
+    //     var markerHeight = 50, markerRadius = 10, linearOffset = 25;
+    //     var popupOffsets = {
+    //         'top': [0, 0],
+    //         'top-left': [0, 0],
+    //         'top-right': [0, 0],
+    //         'bottom': [0, -markerHeight],
+    //         'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    //         'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    //         'left': [markerRadius, (markerHeight - markerRadius) * -1],
+    //         'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+    //     };
+    //
+    //     var popupContent = "<h1>Hello World!</h1>";
+    //
+    //     var popup = new mapboxgl.Popup({closeOnClick: false, offset: popupOffsets})
+    //         .setLngLat(features.geometry.coordinates)
+    //         .setHTML(popupContent)
+    //         .addTo(map);
+    // });
 
     // document.getElementById('export').onclick = function(e) {
     //     // Extract GeoJson from featureGroup
@@ -79,11 +120,20 @@ map.on('load', function(){
     getData(datasetId);
 
 
+
+
+
 });
 
-map.on('draw.update', function(e){alert('test')});
-map.on('draw.render', function(e){alert('test2')});
-map.on('draw.modechange', function(e){alert('test3')});
+map.on('draw.create', function(e) {$('#submitModal').modal('show');
+    console.log(e);
+
+
+
+
+
+
+});
 
 function setFeatureId(){
     return getData(datasetId);
@@ -97,7 +147,9 @@ function getData(datasetId) {
     })
         .done(function(oldData){
             geoJsonFeatures = oldData;
-            source.setData(geoJsonFeatures);
+            //source.setData(geoJsonFeatures);
+            map.getSource('dataset').setData(geoJsonFeatures);
+
             return geoJsonFeatures;
         });
 }
